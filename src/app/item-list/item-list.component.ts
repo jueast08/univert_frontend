8051f0f2bda9891d44bca034fad12fc6ad82d88e
Hugo TESSIER <hugo.tessier@ecole.ensicaen.fr>
2019-01-16 11:43:22 +0100

    Added item and item-list

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-item-list',
-  templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.css']
+    selector: 'app-item-list',
+    templateUrl: './item-list.component.html',
+    styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
 
-  constructor() { }
+    itemListContent = [];
+    
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+	this.itemListContent = [];
+    }
+    
 }
