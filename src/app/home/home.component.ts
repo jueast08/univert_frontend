ce2d1314c82a03c787bb9fa1ae2e9a0c678510ce
Julian EASTERLY <julianeasterly@gmail.com>
2019-01-16 17:21:10 +0100

    created socket for nfc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,16 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { FloatingMapComponent } from '../floating-map/floating-map.component';
 
+import io from "socket.io-client";
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+export class HomeComponent implements OnInit {
+  private url = 'http://localhost:3000';
+  private socket = null;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.socket = io.connect(this.url);
+    console.log('connected');
+    this.socket.on('message', function(msg){
+      console.log(msg);
+    });
   }
 
 }
