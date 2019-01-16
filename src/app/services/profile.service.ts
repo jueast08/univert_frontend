584f58cfe3ab6586827524e3aaf9bcde56a30ad6
Antoine BUHOT <buhot.a@gmail.com>
2019-01-16 18:03:49 +0100

    Fix merge conflict

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -12,7 +12,11 @@ export const PROFILE: Profile = {
     icon : "https://www.forceplus.com/wp-content/uploads/2016/08/medal-1.png", 
     name : "Maître arroseur", 
     description : "A accompli 50 quêtes d'arrosage."
-  }]
+  }],
+	items: [{icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcucQzG-Trh4A11iJG4XP7W7IqibNkRNzWv80i66EO3XiRLsEi",
+		 name: "Pelle",
+		 description: "Pelle de jardinage de base, pour creuser des trous."
+        }]
 }
 
 
