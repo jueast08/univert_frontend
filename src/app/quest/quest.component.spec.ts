7c8bf4026f27455e86d3a40f508ccf9ee2bb6a78
Hugo TESSIER <hugo.tessier@ecole.ensicaen.fr>
2019-01-16 10:55:12 +0100

    Added quest component and its content

--- src/app/quest/quest.component.spec.ts
+++ src/app/quest/quest.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { QuestComponent } from './quest.component';
 
 describe('QuestComponent', () => {
-  let component: QuestComponent;
-  let fixture: ComponentFixture<QuestComponent>;
+    let component: QuestComponent;
+    let fixture: ComponentFixture<QuestComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ QuestComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+	TestBed.configureTestingModule({
+	    declarations: [ QuestComponent ]
+	})
+	    .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(QuestComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+	fixture = TestBed.createComponent(QuestComponent);
+	component = fixture.componentInstance;
+	fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+	expect(component).toBeTruthy();
+    });
 });
--- src/app/quest/quest.component.ts
+++ src/app/quest/quest.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-quest',
-  templateUrl: './quest.component.html',
-  styleUrls: ['./quest.component.css']
+    selector: 'app-quest',
+    templateUrl: './quest.component.html',
+    styleUrls: ['./quest.component.css']
 })
 export class QuestComponent implements OnInit {
 
-  constructor() { }
+    questName = "";
+    questStatus = "";
+    questText = "";
+    questIcon = "";
+    
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+	this.questName = "Arroser";
+	this.questStatus = "À faire";
+	this.questText = "Arroser les plantes";
+	this.questIcon = "https://previews.123rf.com/images/ylivdesign/ylivdesign1607/ylivdesign160700348/59386368-arrosoir-pour-l-ic%C3%B4ne-de-jardin-dans-le-style-de-dessin-anim%C3%A9-isol%C3%A9-sur-fond-blanc-symbole-de-jardinage.jpg";
+    }
 
 }
