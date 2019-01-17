3c9ceea7ca536584634dd1818b6d383a0309a7c3
Antoine BUHOT <buhot.a@gmail.com>
2019-01-17 18:37:35 +0100

    wait for connexion on garden view

--- src/app/garden/garden.component.ts
+++ src/app/garden/garden.component.ts
@@ -4,6 +4,7 @@ import { QuestListComponent } from '../quest-list/quest-list.component';
 import { QuestList } from '../model/quest-list';
 import { QuestService } from '../services/quest.service';
 import { AddQuestFormComponent } from '../add-quest-form/add-quest-form.component';
+import { NfcConnectionService } from '../services/nfc-connection.service';
 
 @Component({
     selector: 'app-garden',
@@ -15,13 +16,15 @@ export class GardenComponent implements OnInit {
     questList: QuestList;
     context = "garden";
 
-    constructor(public questService: QuestService) { }
+    constructor(public questService: QuestService,
+        public nfcConnectionService: NfcConnectionService) { }
 
     ngOnInit() {
         this.questService.getQuestsForGarden("1").subscribe(list => {
             this.questList = list;
             console.log(this.questList)
         });
+        this.nfcConnectionService.init();
     }
 
 
